ce847f4e19aefb5cf03e145b13510a260ad649e6
spite <the.spite@gmail.com>
2014-04-12 20:41:58 +0200

    update LEDPass

--- Wagner.base.js
+++ Wagner.base.js
@@ -606,15 +606,34 @@ WAGNER.LEDPass = function() {
 	WAGNER.Pass.call( this );
 	WAGNER.log( 'LEDPass Pass constructor' );
 	this.loadShader( 'led-fs.glsl', function() {
-		this.shader.uniforms.offset_red.value.set( 0, 0 );
-		this.shader.uniforms.offset_green.value.set( 0, 0 );
-		this.shader.uniforms.offset_blue.value.set( 0, 0 );
-		this.shader.uniforms.dotdistance.value = 5;
+		
+		//this.shader.uniforms.noiseTexture.value = 1;
 	} );
 
+	this.params.pixelSize = 10;
+	this.params.tolerance = .25;
+	this.params.pixelRadius = .25;
+	this.params.luminanceSteps = 100;
+	this.params.luminanceBoost = .2;
+	this.params.colorBoost = .01;
+	this.params.burntOutPercent = 50;
 };
 
 WAGNER.LEDPass.prototype = Object.create( WAGNER.Pass.prototype );
+
+WAGNER.LEDPass.prototype.run = function( c ) {
+
+	this.shader.uniforms.pixelSize.value = this.params.pixelSize;
+	this.shader.uniforms.tolerance.value = this.params.tolerance;
+	this.shader.uniforms.pixelRadius.value = this.params.pixelRadius;
+	this.shader.uniforms.luminanceSteps.value = this.params.luminanceSteps;
+	this.shader.uniforms.luminanceBoost.value = this.params.luminanceBoost;
+	this.shader.uniforms.colorBoost.value = this.params.colorBoost;
+	this.shader.uniforms.burntOutPercent.value = this.params.burntOutPercent;
+
+	c.pass( this.shader );
+
+}
 
 WAGNER.HalftonePass = function() {
 
