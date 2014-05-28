6796dc09a810c504bb0d677ef5df977fa0bec9b4
spite <the.spite@gmail.com>
2014-05-28 21:18:42 +0200

    removed vPosition varying from vertex shader

--- vertex-shaders/orto-vs.glsl
+++ vertex-shaders/orto-vs.glsl
@@ -1,10 +1,9 @@
 varying vec2 vUv;
-varying vec3 vPosition;
 
 void main() {
 
 	vUv = uv;
-	vPosition = vec4( modelViewMatrix * vec4( position, 1.0 ) ).xyz;
+//	vPosition = vec4( modelViewMatrix * vec4( position, 1.0 ) ).xyz;
 	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
 }
