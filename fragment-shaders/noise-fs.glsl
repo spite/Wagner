f7ad7d30f040ce0d0d01e98a2a9164f261017832
Jaume Sanchez <the.spite@gmail.com>
2015-01-27 00:29:33 +0100

    fix random function for noise pass on tablet/mobile

--- fragment-shaders/noise-fs.glsl
+++ fragment-shaders/noise-fs.glsl
@@ -4,14 +4,18 @@ uniform float speed;
 uniform float time;
 varying vec2 vUv;
 
-float random(vec3 scale,float seed){return fract(sin(dot(gl_FragCoord.xyz+seed,scale))*43758.5453+seed);}
+float random(vec2 n, float offset ){
+	//return fract(sin(dot(gl_FragCoord.xyz+seed,scale))*43758.5453);
+	return .5 - fract(sin(dot(n.xy + vec2( offset, 0. ), vec2(12.9898, 78.233)))* 43758.5453);
+}
 
 void main() {
 
 	vec4 color = texture2D(tInput, vUv);
 
-	color += amount * ( .5 - random( vec3( 1. ), length( gl_FragCoord ) + speed * .01 * time ) );
-	
+	//color += amount * ( .5 - random( vec3( 1. ), length( gl_FragCoord ) + speed * .01 * time ) );
+	color += vec4( vec3( amount * random( vUv, .00001 * speed * time ) ), 1. );
+
 	gl_FragColor = color;
 
 }
