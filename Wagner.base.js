a0474c94aa7973c559e8d58f2a55194d4ba702e8
Jaume Sanchez <the.spite@gmail.com>
2015-02-07 00:44:10 +0100

    added brightness contest pass

--- Wagner.base.js
+++ Wagner.base.js
@@ -83,6 +83,28 @@ WAGNER.SepiaPass.prototype.run = function( c ) {
 
 }
 
+WAGNER.BrightnessContrastPass = function() {
+
+	WAGNER.Pass.call( this );
+	WAGNER.log( 'BrightnessContrastPass constructor' );
+	this.loadShader( 'brightness-contrast-fs.glsl' );
+
+	this.params.brightness = 1;
+	this.params.contrast = 1;
+
+};
+
+WAGNER.BrightnessContrastPass.prototype = Object.create( WAGNER.Pass.prototype );
+
+WAGNER.BrightnessContrastPass.prototype.run = function( c ) {
+
+	this.shader.uniforms.brightness.value = this.params.brightness;
+	this.shader.uniforms.contrast.value = this.params.contrast;
+
+	c.pass( this.shader );
+
+}
+
 WAGNER.Pass.prototype.bindUniform = function( p, s, v, c ) {
 
 	Object.defineProperty( p, v, { 
